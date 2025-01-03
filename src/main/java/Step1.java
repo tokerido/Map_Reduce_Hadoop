4a8c6620043379ee7608254ab11b6fd39f34504d
lizgo99 <liz.gokhvat@gmail.com>
2025-01-03 05:11:49 +0200

    replace WordCount with Step1

--- src/main/java/Step1.java
+++ src/main/java/Step1.java
@@ -1,5 +1,4 @@
 
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 //import org.apache.hadoop.io.IntWritable;
@@ -31,7 +30,7 @@ import java.net.URI;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 
-public class WordCount {
+public class Step1 {
     public static class MapperClass extends Mapper<LongWritable, Text, Text, Text> {
         public final HashSet<String> STOPWORDS = new HashSet<>();
         private String currentFileName;
@@ -39,13 +38,20 @@ public class WordCount {
         @Override
         protected void setup(Context context) {
             Collections.addAll(STOPWORDS,
-                    "״", "׳", "של", "רב", "פי", "עם", "עליו", "עליהם", "על", "עד", "מן", "מכל", "מי", "מהם", "מה", "מ", "למה", "לכל", "לי", "לו",
-                    "להיות", "לה", "לא", "כן", "כמה", "כלי", "כל", "כי", "יש", "ימים", "יותר", "יד", "י", "זה", "ז", "ועל", "ומי", "ולא", "וכן", "וכל", "והיא",
-                    "והוא", "ואם", "ו", "הרבה", "הנה", "היו", "היה", "היא", "הזה", "הוא", "דבר", "ד", "ג", "בני", "בכל", "בו", "בה", "בא", "את", "אשר", "אם",
-                    "אלה", "אל", "אך", "איש", "אין", "אחת", "אחר", "אחד", "אז", "אותו", "־", "^", "?", ";", ":", "1", ".", "-", "*", "\"", "!", "שלשה", "בעל",
-                    "פני", ")", "גדול", "שם", "עלי", "עולם", "מקום", "לעולם", "לנו", "להם", "ישראל", "יודע", "זאת", "השמים", "הזאת", "הדברים", "הדבר", "הבית",
-                    "האמת", "דברי", "במקום", "בהם", "אמרו", "אינם", "אחרי", "אותם", "אדם", "(", "חלק", "שני", "שכל", "שאר", "ש", "ר", "פעמים", "נעשה", "ן", "ממנו",
-                    "מלא", "מזה", "ם", "לפי", "ל", "כמו", "כבר", "כ", "זו", "ומה", "ולכל", "ובין", "ואין", "הן", "היתה", "הא", "ה", "בל", "בין", "בזה", "ב", "אף",
+                    "״", "׳", "של", "רב", "פי", "עם", "עליו", "עליהם", "על", "עד", "מן", "מכל", "מי", "מהם", "מה", "מ",
+                    "למה", "לכל", "לי", "לו",
+                    "להיות", "לה", "לא", "כן", "כמה", "כלי", "כל", "כי", "יש", "ימים", "יותר", "יד", "י", "זה", "ז",
+                    "ועל", "ומי", "ולא", "וכן", "וכל", "והיא",
+                    "והוא", "ואם", "ו", "הרבה", "הנה", "היו", "היה", "היא", "הזה", "הוא", "דבר", "ד", "ג", "בני", "בכל",
+                    "בו", "בה", "בא", "את", "אשר", "אם",
+                    "אלה", "אל", "אך", "איש", "אין", "אחת", "אחר", "אחד", "אז", "אותו", "־", "^", "?", ";", ":", "1",
+                    ".", "-", "*", "\"", "!", "שלשה", "בעל",
+                    "פני", ")", "גדול", "שם", "עלי", "עולם", "מקום", "לעולם", "לנו", "להם", "ישראל", "יודע", "זאת",
+                    "השמים", "הזאת", "הדברים", "הדבר", "הבית",
+                    "האמת", "דברי", "במקום", "בהם", "אמרו", "אינם", "אחרי", "אותם", "אדם", "(", "חלק", "שני", "שכל",
+                    "שאר", "ש", "ר", "פעמים", "נעשה", "ן", "ממנו",
+                    "מלא", "מזה", "ם", "לפי", "ל", "כמו", "כבר", "כ", "זו", "ומה", "ולכל", "ובין", "ואין", "הן", "היתה",
+                    "הא", "ה", "בל", "בין", "בזה", "ב", "אף",
                     "אי", "אותה", "או", "אבל", "א", "");
 
             FileSplit fileSplit = (FileSplit) context.getInputSplit();
@@ -54,21 +60,25 @@ public class WordCount {
 
         }
 
-        protected boolean isValuable(String[] words, int nGramSize){
+        protected boolean isValuable(String[] words, int nGramSize, Context context) {
 
-            if (words.length != nGramSize){
+            if (words.length != nGramSize) {
                 return false;
             }
 
-            for (String word : words){
-                if (word == null || STOPWORDS.contains(word)){
+            for (int i = 0; i < words.length; i++) {
+                if (words[i] == null) {
+                    return false;
+                }
+                if (words[i].startsWith("\"") && words[i].length() > 1) {
+                    words[i] = words[i].substring(1);
+                }
+                if (STOPWORDS.contains(words[i])) {
                     return false;
                 }
             }
-
             return true;
         }
-
 
         @Override
         public void map(LongWritable lineId, Text line, Context context) throws IOException, InterruptedException {
@@ -77,14 +87,17 @@ public class WordCount {
 
             if (fields.length > 0) {
                 String[] words = fields[0].split(" ");
-                if (isValuable(words, words.length)) {
-                    context.write(new Text(fields[0]), new Text(fields[2]));
+//                boolean isValuable = ;
+//                context.write(new Text("This is the N-gram - " + fields[0] + " - This is the length - " + words.length + " - isValuable:" + isValuable),
+//                            new Text("0"));
+                if (isValuable(words, words.length, context)) {
+                    context.write(new Text(String.join(" ", words)), new Text(fields[2]));
                     if (words.length == 1) {
-//                        context.write(new Text("C0"), new Text(fields[2]));
+                        // context.write(new Text("C0"), new Text(fields[2]));
                         context.getCounter("CustomGroup", "C0").increment(Integer.parseInt(fields[2]));
                     }
                 }
-            }else{
+            } else {
                 System.out.println("fields length is 0");
             }
         }
@@ -93,8 +106,7 @@ public class WordCount {
     public static class ReducerClass extends Reducer<Text, Text, Text, Text> {
 
         private MultipleOutputs<Text, Text> multipleOutputs;
-        private long totalWords = 0;
-
+        // private long totalWords = 0;
 
         @Override
         protected void setup(Context context) throws IOException, InterruptedException {
@@ -104,12 +116,12 @@ public class WordCount {
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-            if (key.toString().equals("C0")) {
-                for (Text value : values) {
-                    totalWords += Long.parseLong(value.toString());
-                }
-                return;
-            }
+            // if (key.toString().equals("C0")) {
+            // for (Text value : values) {
+            // totalWords += Long.parseLong(value.toString());
+            // }
+            // return;
+            // }
 
             String[] words = key.toString().split(" ");
 
@@ -130,75 +142,35 @@ public class WordCount {
                     output = "3gram";
                     break;
                 default:
-                    output = "1gram"; // Default case
+                    output = "debug"; // Default case
                     break;
             }
 
             multipleOutputs.write(output, key, new Text(Integer.toString(sum)));
-//            context.write(key, new Text(Integer.toString(sum)));
+            // context.write(key, new Text(Integer.toString(sum)));
         }
 
         @Override
         protected void cleanup(Context context) throws IOException, InterruptedException {
-            multipleOutputs.write("1gram", new Text("C0"), new Text(Long.toString(totalWords)));
+            // multipleOutputs.write("1gram", new Text("C0"), new
+            // Text(Long.toString(totalWords)));
             multipleOutputs.close();
         }
     }
 
     public static class CombinerClass extends Reducer<Text, Text, Text, Text> {
 
-//        private MultipleOutputs<Text, Text> multipleOutputs;
-
-        @Override
-        protected void setup(Context context) throws IOException, InterruptedException {
-//            multipleOutputs = new MultipleOutputs<>(context);
-        }
-
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
-
-            if (key.toString().equals("C0")) {
-                long wordTotal = 0;
-                for (Text value : values) {
-                    wordTotal += Long.parseLong(value.toString());
-                }
-                context.write(key, new Text(Long.toString(wordTotal)));
-                return;
-            }
-
-            String[] words = key.toString().split(" ");
 
             int sum = 0;
             for (Text value : values) {
                 sum += Integer.parseInt(value.toString());
             }
 
-//            String output;
-//            switch (words.length) {
-//                case 1:
-//                    output = "1gram";
-//                    break;
-//                case 2:
-//                    output = "2gram";
-//                    break;
-//                case 3:
-//                    output = "3gram";
-//                    break;
-//                default:
-//                    output = "1gram"; // Default case
-//                    break;
-//            }
-
-
-//            multipleOutputs.write(output, key, new Text(Integer.toString(sum)));
             context.write(key, new Text(Integer.toString(sum)));
         }
 
-        @Override
-        protected void cleanup(Context context) throws IOException, InterruptedException {
-//            multipleOutputs.close();
-        }
     }
 
     public static class PartitionerClass extends Partitioner<Text, Text> {
@@ -217,11 +189,14 @@ public class WordCount {
 
     public static void main(String[] args) throws Exception {
         System.out.println("[DEBUG] STEP 1 started!");
-        System.out.println(args.length > 0 ? args[0] : "no args");
+
+        // String jarBucketName = "jarbucket1012";
+        String jarBucketName = "hadoop-map-reduce-bucket";
+
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "Step1");
 
-        job.setJarByClass(WordCount.class);
+        job.setJarByClass(Step1.class);
         job.setMapperClass(MapperClass.class);
         job.setReducerClass(ReducerClass.class);
         job.setPartitionerClass(PartitionerClass.class);
@@ -231,36 +206,40 @@ public class WordCount {
         job.setOutputValueClass(Text.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
-        job.setInputFormatClass(SequenceFileInputFormat.class);  //For S3 Ngram data
 //        job.setInputFormatClass(TextInputFormat.class);
+//        FileInputFormat.addInputPath(job, new Path("s3://" + jarBucketName + "/input/"));
 
+        job.setInputFormatClass(SequenceFileInputFormat.class); // For S3 Ngram data
         FileInputFormat.addInputPath(job,
                 new Path("s3://datasets.elasticmapreduce/ngrams/books/20090715/heb-all/3gram/data"));
         FileInputFormat.addInputPath(job,
                 new Path("s3://datasets.elasticmapreduce/ngrams/books/20090715/heb-all/2gram/data"));
-//
+        //
         FileInputFormat.addInputPath(job,
                 new Path("s3://datasets.elasticmapreduce/ngrams/books/20090715/heb-all/1gram/data"));
-//
-//        FileInputFormat.addInputPath(job, new Path("s3://bucketforjars/input/3gram_sample_100"));
-//        FileOutputFormat.setOutputPath(job, new Path("s3://bucketforjars/output_word_count"));
 
-//        conf.set("mapreduce.input.fileinputformat.split.maxsize", "128000000");
+
+
+
+        // conf.set("mapreduce.input.fileinputformat.split.maxsize", "128000000");
 
         // Enable MultipleOutputs
         MultipleOutputs.addNamedOutput(job, "1gram", TextOutputFormat.class, Text.class, Text.class);
         MultipleOutputs.addNamedOutput(job, "2gram", TextOutputFormat.class, Text.class, Text.class);
         MultipleOutputs.addNamedOutput(job, "3gram", TextOutputFormat.class, Text.class, Text.class);
+        MultipleOutputs.addNamedOutput(job, "debug", TextOutputFormat.class, Text.class, Text.class);
 
+        // FileInputFormat.addInputPath(job, new Path(args[1]));
+        // FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
-//        FileInputFormat.addInputPath(job, new Path(args[1]));
-//        FileOutputFormat.setOutputPath(job, new Path(args[2]));
-
-//        FileInputFormat.addInputPath(job, new Path("s3://jarbucket1012/input-samples/1gram_sample.csv"));
-//        FileInputFormat.addInputPath(job, new Path("s3://jarbucket1012/input-samples/2gram_sample.csv"));
-//        FileInputFormat.addInputPath(job, new Path("s3://jarbucket1012/input-samples/3gram_sample.csv"));
-//        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        FileOutputFormat.setOutputPath(job, new Path("s3://jarbucket1012/step1_output/"));
+        // FileInputFormat.addInputPath(job, new Path("s3://" + jarBucketName +
+        // "/input-samples/1gram_sample.csv"));
+        // FileInputFormat.addInputPath(job, new Path("s3://" + jarBucketName +
+        // "/input-samples/2gram_sample.csv"));
+        // FileInputFormat.addInputPath(job, new Path("s3://" + jarBucketName +
+        // "/input-samples/3gram_sample.csv"));
+        // FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path("s3://" + jarBucketName + "/step1_output/"));
 
         boolean success = job.waitForCompletion(true);
 
@@ -271,9 +250,11 @@ public class WordCount {
                     .getValue();
 
             // Write the counter value to the beginning of the file
-            String s3OutputPath = "s3://jarbucket1012/step1_output/part-r-00000"; // S3 location, e.g., "s3://my-bucket/counter-output.txt"
+            String s3OutputPath = "s3://" + jarBucketName + "/step1_output/part-r-00000"; // S3 location, e.g.,
+                                                                                          // "s3://my-bucket/counter-output.txt"
             FileSystem fs = FileSystem.get(URI.create(s3OutputPath), new Configuration());
-            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fs.create(new Path(s3OutputPath), true)))) {
+            try (BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(fs.create(new Path(s3OutputPath), true)))) {
                 writer.write("C0 " + c0Value);
                 writer.newLine(); // Add a newline if you plan to append other data later
             }
@@ -282,4 +263,3 @@ public class WordCount {
     }
 
 }
-
